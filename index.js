d08147a681b4fbbc02b90526babbe43eee72414e
Brendan Conard <117928315+Brendanthatman@users.noreply.github.com>
2024-12-04 20:25:41 +0000

    js frfr

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-const url = 'https://open-weather13.p.rapidapi.com/city/latlon/43.119/-85.559';
 const options = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': 'ab6c56b840msh00dd6e776e7e18ep13c302jsncbe8de164cb2',
-		'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
+		'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com',
+		'X-RapidAPI-Key': 'ab6c56b840msh00dd6e776e7e18ep13c302jsncbe8de164cb2'
 	}
 };
-
-try {
-	const response = fetch(url, options);
-	const result = response.json();
-	console.log(result);
-
-	const data = result.results;
-
-	let city = result.name;	
-
-	document.getElementsByClassName('lineone').innerHTML = city;
-
-
-} catch (error) {
-	console.error(error);
-}
+fetch('https://open-weather13.p.rapidapi.com/city/latlon/43.119/-85.559', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
