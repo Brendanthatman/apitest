7ddc2bc757a6c2070c37a6fa1572fd899c42f8b0
Brendan Conard <117928315+Brendanthatman@users.noreply.github.com>
2024-12-04 20:09:12 +0000

    js

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const data = null;
-
-const xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.addEventListener('readystatechange', function () {
-	if (this.readyState === this.DONE) {
-		console.log(this.responseText);
+const url = 'https://open-weather13.p.rapidapi.com/city/latlon/43.119/-85.559';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'ab6c56b840msh00dd6e776e7e18ep13c302jsncbe8de164cb2',
+		'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
 	}
-});
+};
 
-xhr.open('GET', 'https://zillow-com1.p.rapidapi.com/property?zpid=2080998890');
-xhr.setRequestHeader('x-rapidapi-key', 'ab6c56b840msh00dd6e776e7e18ep13c302jsncbe8de164cb2');
-xhr.setRequestHeader('x-rapidapi-host', 'zillow-com1.p.rapidapi.com');
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+	console.log(result.results);
 
-xhr.send(data);
+	const data = result.results;
+
+	let city = data.name;	
+
+	document.getElementsByClassName('lineone').innerHTML = city;
+
+
+} catch (error) {
+	console.error(error);
+}
