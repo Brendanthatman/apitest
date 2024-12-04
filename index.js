c7d05aca0c3d084ff0a0cb66acfed66950777dcf
Brendan Conard <117928315+Brendanthatman@users.noreply.github.com>
2024-12-04 20:46:18 +0000

    test weather again

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com',
+		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
 		'X-RapidAPI-Key': 'ab6c56b840msh00dd6e776e7e18ep13c302jsncbe8de164cb2'
 	}
 };
-fetch('https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=43.119&lon=-85.559&units=imperial&lang=en', options)
+fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=43.119%2C-85.559', options)
 	.then(response => response.json())
 	.then(response => {
 		console.log(response)
-
+		
+		document.getElementsByClassName('lineone').innerHTML = response.city_name;
 	})
 	.catch(err => console.error(err));
